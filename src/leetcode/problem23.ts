a5d570520e64af0ce1444ec74a567394bdeecbaa
hikovsky <ospanovkuanyshbek@gmail.com>
2023-07-02 14:20:04 +0600

    Problem 23: Merge k Sorted Lists

---
+++ src/leetcode/problem23.ts
@@ -0,0 +1,34 @@
+/**
+ * 23. Merge k Sorted Lists
+ * Link to problem: https://leetcode.com/problems/merge-k-sorted-lists/
+ */
+import {ListNode} from "../utils/linkedList";
+
+function arrayToList(array: number[]): ListNode {
+    let head = new ListNode(array[0])
+
+    let current = head
+    for (let i = 1; i < array.length; i++) {
+        current.next = new ListNode(array[i])
+        current = current.next
+    }
+
+    return head
+}
+
+export function mergeKLists(lists: Array<ListNode | null>): ListNode | null {
+    let array = []
+
+    for (let list of lists) {
+        while(list) {
+            array.push(list.val)
+            list = list.next
+        }
+    }
+
+    if (!array.length) return null
+
+    array = array.sort((a,b) => a - b)
+
+    return arrayToList(array)
+};
